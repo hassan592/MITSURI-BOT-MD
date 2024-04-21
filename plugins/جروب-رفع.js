ecc11e0abfef8a0ae02ff582010a9ecbb18da4af
Mohamed Sharqawi <165744215+SUNGX-MD@users.noreply.github.com>
2024-04-21 08:29:58 +0200

    Update and rename ترقيه.js to جروب-رفع.js

---
+++ plugins/جروب-رفع.js
@@ -0,0 +1,38 @@
+var handler = async (m, { conn,usedPrefix, command, text }) => {
+
+if (isNaN(text) && !text.match(/@/g)){
+	
+} else if (isNaN(text)) {
+var number = text.split`@`[1]
+} else if (!isNaN(text)) {
+var number = text
+}
+
+if (!text && !m.quoted) return conn.reply(m.chat, `*ضع علامة على المستخدم. من يريد الترقية إلى مشرف*`, m, fake, )
+if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*⌝❌⌞ حدث خطأ*`, m, fake, )
+  
+try {
+if (text) {
+var user = number + '@s.whatsapp.net'
+} else if (m.quoted.sender) {
+var user = m.quoted.sender
+} else if (m.mentionedJid) {
+var user = number + '@s.whatsapp.net'
+} 
+} catch (e) {
+} finally {
+conn.groupParticipantsUpdate(m.chat, [user], 'promote')
+conn.reply(m.chat, `*⌝☑️⌞ تمت ترقيته إلى المشرف بنجاح*`, m, fake, )
+}
+
+}
+handler.help = ['رفع']
+handler.tags = ['جروب']
+handler.command = ['رفع', 'ترقية']
+
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+
+export default handler
